e35d1172befecb64b965125c7cb614f843a99789
frost-byte <brian@frost-byte.net>
2026-01-17 21:30:05 -0500

    Prepare to publish to Comfy Registry

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ __all__ = [
     "WEB_DIRECTORY",
 ]
 
-__author__ = """comfyui-fbTools"""
+__author__ = """frost-byte"""
 __email__ = "brian@frost-byte.net"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .extension import FBToolsExtension
 async def comfy_entrypoint() -> FBToolsExtension:
